f99fcf9934be676f0ba3de394a132ccd5728320f
Roberto <ra48273551857@acaonsfatima.org.br>
2024-08-02 11:55:32 -0300

    vaiii

--- app.js
+++ app.js
@@ -9,5 +9,12 @@ server.get('/calculadora/soma', (req, resp) => {
     })
 })
 
+server.get("/calculadora/subitracao",(req,resp)=>{
+    let n1 = Number(req.query.n1)
+    let n2 =Number(req.query.n2)
+    let subi = n1-n2
+    resp.send(subi)
+})
+
 
 server.listen(5050, () => console.log('ta subindo ein'))
